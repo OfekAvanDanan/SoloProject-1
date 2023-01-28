bdcc39aa89963640d6426177c686e7dd800394e0
OfekAvanDanan <44505560+OfekAvanDanan@users.noreply.github.com>
2023-01-28 18:12:58 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { ReactSVG } from 'react-svg'
+import {userProfile} from './userProfile.js';
+
+import './App.css';
+
+import profilePicture  from './images/profilePicture.png';
+import FacebookIcon  from './images/Facebook Icon.svg';
+import gitHubIcon from './images/GitHub Icon.svg';
+import instagramIcon from './images/Instagram Icon.svg';
+import mailIcon from './images/Mail.svg';
+import twitterIcon from './images/Twitter Icon.svg';
+import LinkedinIcon from './images/Linked in.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Window">
+        <img src={profilePicture} className="profile-photo" alt="profile"/>
+        <h1>{userProfile.name}</h1>
+        <h3>{userProfile.proffesion}</h3>
+        <h4>{userProfile.websiteAdress}</h4>
+        <div className="buttons">
+          <button id='butn Email'> <ReactSVG src={mailIcon} /> Email</button>
+          <button id='button-LinkedIn'> <ReactSVG src={LinkedinIcon} /> Linkedin</button>
+        </div>
+        <div className='Description'>
+          <h2>About</h2>
+          <p>{userProfile.about}</p>
+          <h2>Interests</h2>
+          <p>{userProfile.interests}</p>
+        </div>
+        <div className='icons'>
+          <ReactSVG src={twitterIcon} alt="twitter icon" />
+          <ReactSVG src={FacebookIcon} alt="Facebook icon"/>
+          <ReactSVG src={instagramIcon} alt="Instagram icon"/>
+          <ReactSVG src={gitHubIcon} alt="GitHab icon"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/userProfile.js
@@ -0,0 +1,9 @@
+export const userProfile = {
+    name: "Laura Smith",
+    proffesion: "Frontend Developer",
+    profilePicturePath:  ".images/profilePicture.png",
+    websiteAdress: "laurasmith.website",
+    about: "I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.",
+    interests: "Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.",
+   };
+   
